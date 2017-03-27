d2bddd00373d2b270bdfa428bb2e79b1894da1e2
agessner <ajgessner@hotmail.com>
2017-03-26 22:46:37 -0300

    Validação de email

--- emberjs/app/routes/analyzer/steps/2.js
+++ emberjs/app/routes/analyzer/steps/2.js
@@ -6,13 +6,22 @@ export default Ember.Route.extend({
         this._super(controller, model);
         
     },  
+    
+    validateEmail(email) {
+        var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        return re.test(email);
+    },
 
     actions: {
 
         foward() {
             
             if (!this.controller.get('model').get('userName') || !this.controller.get('model').get('userEmail')) {
-                return Ember.get(this, 'flashMessages').danger('Nome e emai devem ser informados!');
+                return Ember.get(this, 'flashMessages').danger('Nome e email devem ser informados!');
+            }
+
+            if (!this.validateEmail(this.controller.get('model').get('userEmail'))) {
+                return Ember.get(this, 'flashMessages').danger('Email Inválido!');
             }
 
             this.transitionTo('analyzer.steps.3');
